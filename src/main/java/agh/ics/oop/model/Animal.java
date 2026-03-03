a0d3c68a85fdcd5cc1932e34464d14bb92117329
MacApos <maciejapostol98@gmail.com>
2026-03-04 00:45:49 +0100

    reformat

--- src/main/java/agh/ics/oop/model/Animal.java
+++ src/main/java/agh/ics/oop/model/Animal.java
@@ -38,14 +38,14 @@ public class Animal {
             case FORWARD -> {
                 Vector2d to_add = MapDirection.toUnitVector(orientation);
                 if (position.add(to_add).getX() <= 4 && position.add(to_add).getY() <= 4 &&
-                    position.add(to_add).getX() >= 0 && position.add(to_add).getY() >= 0) {
+                        position.add(to_add).getX() >= 0 && position.add(to_add).getY() >= 0) {
                     position = position.add(to_add);
                 }
             }
             case BACKWARD -> {
                 Vector2d to_sub = MapDirection.toUnitVector(orientation);
                 if (position.subtract(to_sub).getX() <= 4 && position.subtract(to_sub).getY() <= 4 &&
-                    position.subtract(to_sub).getX() >= 0 && position.subtract(to_sub).getY() >= 0) {
+                        position.subtract(to_sub).getX() >= 0 && position.subtract(to_sub).getY() >= 0) {
                     position = position.subtract(to_sub);
                 }
             }
